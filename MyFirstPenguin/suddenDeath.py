0857a00c8f5cb520dba3c7676660b44b56736112
Martin Bjerke <mgbjerke@gmail.com>
2018-11-06 21:20:13 +0100

    testing in master

--- MyFirstPenguin/suddenDeath.py
+++ MyFirstPenguin/suddenDeath.py
@@ -1,7 +1,7 @@
 import math
 
-from MyFirstPenguin.eucDist import eucDistance as eucDist
-from MyFirstPenguin.run import moveTowardsPoint as moveTowardsPoint
+from eucDist import eucDistance as eucDist
+from run import moveTowardsPoint as moveTowardsPoint
 
 ROTATE_LEFT = "rotate-left"
 ROTATE_RIGHT = "rotate-right"
